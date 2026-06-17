482bcf4f924c9ddd1500f043c11b97329797e757
Yustina Y <167534397+yustinayunitayy@users.noreply.github.com>
2026-06-17 10:50:42 +0700

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
+import vercel from '@astrojs/vercel'
 
 const env = loadEnv('', process.cwd(), '')
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   site: env.SITE_URL,
-
   i18n: {
     defaultLocale: 'id',
     locales: ['id', 'en'],
